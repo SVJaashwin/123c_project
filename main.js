83e1e0683cd6644b93555810bb8154cbc3893dec
SVJaashwin <88593780+SVJaashwin@users.noreply.github.com>
2022-04-14 17:29:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-//
+function setup() {
+    canvas = createCanvas(350, 350);
+    canvas.position(900, 140);
+    video = createCapture(VIDEO);
+    video.size(350, 350);
+    posenet = ml5.poseNet(video, modelLoded);
+    posenet.on("pose", gotPoses);
+}
+
+function modelLoded() {
+    console.log("model_Loded");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results)
+    }
+}
+
+function draw() {
+    background("#cf4011");
+}
